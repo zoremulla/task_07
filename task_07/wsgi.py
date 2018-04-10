e6f46bbfe006af07f34e9bf528985d39f1ee9eb3
DarthHamza <hamzamakia@gmail.com>
2018-04-10 19:27:38 +0300

    rename

--- task_07/wsgi.py
+++ task_07/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for task_06 project.
+WSGI config for task_07 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_06.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_07.settings")
 
 application = get_wsgi_application()
